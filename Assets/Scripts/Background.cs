a09ab51b7100cedd5acd29082bc825915c1662b1
lhumiston <lance.humiston@hp.com>
2017-01-05 00:34:07 -0800

    add .meta files to repo

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class Background : MonoBehaviour {
-	// Use this for initialization
-	GameObject plane;
-	void Start () {
-		plane = GameObject.Find("background");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		plane.transform.Rotate(0, 2 ,0);
-	}
+    // Use this for initialization
+    GameObject plane;
+    void Start () {
+        plane = GameObject.Find("background");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        plane.transform.Rotate(0, 2 ,0);
+    }
 }
